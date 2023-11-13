4e3587a1b3fb8f2d0d2c4f36ba940d88a02ea51d
leikooo <108205879+lieeew@users.noreply.github.com>
2023-11-13 16:29:12 +0800

    add 可以在配置文件中配置 appid、apiKey、 apiSecret 更加人性化

--- src/main/java/io/github/briqt/spark4j/SparkClient.java
+++ src/main/java/io/github/briqt/spark4j/SparkClient.java
@@ -6,9 +6,14 @@ import io.github.briqt.spark4j.listener.SparkBaseListener;
 import io.github.briqt.spark4j.listener.SparkSyncChatListener;
 import io.github.briqt.spark4j.model.SparkSyncChatResponse;
 import io.github.briqt.spark4j.model.request.SparkRequest;
+import io.github.briqt.spark4j.utils.AuthorizationUtils;
+import lombok.Data;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
@@ -17,11 +22,17 @@ import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
+import static io.github.briqt.spark4j.utils.AuthorizationUtils.getAuthUrl;
+
 /**
  * XfSparkClient
  *
  * @author briqt
  */
+@Configuration
+@ConfigurationProperties("xunfei.client")
+@Data
+@ComponentScan
 public class SparkClient {
 
     public String appid;
@@ -42,7 +53,7 @@ public class SparkClient {
         // 构建鉴权url
         String authWsUrl = null;
         try {
-            authWsUrl = getAuthUrl(apiUrl).replace("http://", "ws://").replace("https://", "wss://");
+            authWsUrl = AuthorizationUtils.getAuthUrl(apiUrl, apiSecret, apiKey).replace("http://", "ws://").replace("https://", "wss://");
         } catch (Exception e) {
             throw new SparkException(500, "构建鉴权url失败", e);
         }
@@ -65,37 +76,4 @@ public class SparkClient {
         return chatResponse;
     }
 
-    /**
-     * 获取认证之后的URL
-     */
-    public String getAuthUrl(String apiUrl) throws Exception {
-        URL url = new URL(apiUrl);
-        // 时间
-        SimpleDateFormat format = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z", Locale.US);
-        format.setTimeZone(TimeZone.getTimeZone("GMT"));
-        String date = format.format(new Date());
-        // 拼接
-        String preStr = "host: " + url.getHost() + "\n" +
-                "date: " + date + "\n" +
-                "GET " + url.getPath() + " HTTP/1.1";
-        // SHA256加密
-        Mac mac = Mac.getInstance("hmacsha256");
-        SecretKeySpec spec = new SecretKeySpec(apiSecret.getBytes(StandardCharsets.UTF_8), "hmacsha256");
-        mac.init(spec);
-
-        byte[] hexDigits = mac.doFinal(preStr.getBytes(StandardCharsets.UTF_8));
-        // Base64加密
-        String sha = Base64.getEncoder().encodeToString(hexDigits);
-
-        // 拼接
-        String authorization = String.format("api_key=\"%s\", algorithm=\"%s\", headers=\"%s\", signature=\"%s\"", apiKey, "hmac-sha256", "host date request-line", sha);
-        // 拼接地址
-        HttpUrl httpUrl = Objects.requireNonNull(HttpUrl.parse("https://" + url.getHost() + url.getPath())).newBuilder().//
-                addQueryParameter("authorization", Base64.getEncoder().encodeToString(authorization.getBytes(StandardCharsets.UTF_8))).//
-                addQueryParameter("date", date).//
-                addQueryParameter("host", url.getHost()).//
-                build();
-
-        return httpUrl.toString();
-    }
 }
---
+++ src/main/java/io/github/briqt/spark4j/utils/AuthorizationUtils.java
@@ -0,0 +1,50 @@
+package io.github.briqt.spark4j.utils;
+
+import okhttp3.HttpUrl;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+/**
+ * @author leikooo
+ * @Description
+ */
+public class AuthorizationUtils {
+
+    /**
+     * 获取认证之后的URL
+     */
+    public static String getAuthUrl(String apiUrl, String apiSecret, String apiKey) throws Exception {
+        URL url = new URL(apiUrl);
+        // 时间
+        SimpleDateFormat format = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z", Locale.US);
+        format.setTimeZone(TimeZone.getTimeZone("GMT"));
+        String date = format.format(new Date());
+        // 拼接
+        String preStr = "host: " + url.getHost() + "\n" +
+                "date: " + date + "\n" +
+                "GET " + url.getPath() + " HTTP/1.1";
+        // SHA256加密
+        Mac mac = Mac.getInstance("hmacsha256");
+        SecretKeySpec spec = new SecretKeySpec(apiSecret.getBytes(StandardCharsets.UTF_8), "hmacsha256");
+        mac.init(spec);
+
+        byte[] hexDigits = mac.doFinal(preStr.getBytes(StandardCharsets.UTF_8));
+        // Base64加密
+        String sha = Base64.getEncoder().encodeToString(hexDigits);
+
+        // 拼接
+        String authorization = String.format("api_key=\"%s\", algorithm=\"%s\", headers=\"%s\", signature=\"%s\"", apiKey, "hmac-sha256", "host date request-line", sha);
+        // 拼接地址
+        HttpUrl httpUrl = Objects.requireNonNull(HttpUrl.parse("https://" + url.getHost() + url.getPath())).newBuilder().//
+                addQueryParameter("authorization", Base64.getEncoder().encodeToString(authorization.getBytes(StandardCharsets.UTF_8))).//
+                addQueryParameter("date", date).//
+                addQueryParameter("host", url.getHost()).//
+                build();
+        return httpUrl.toString();
+    }
+}
